db72e90be9858bd923786135feeb5d626b09a89d
DemianDD <thvarichdemyan@icloud.com>
2023-10-17 12:25:57 +0300

    initial react native expo app

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Platform, View, Text, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+import PokemonList from "./screens/PokemonList";
+import MoveList from "./screens/MoveList";
+import PokemonDetail from "./screens/PokemonDetail";
+import MoveDetail from "./screens/MoveDetail";
+
+import pokemonTabIcon from "./assets/pokemon-active.png";
+import moveTabIcon from "./assets/move-active.png";
+
+const PokemonStack = createStackNavigator();
+const MoveStack = createStackNavigator();
+const stackScreenOptions = {
+  headerShown: false,
+  gestureEnabled: true,
+};
+
+function PokemonStackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PokemonStack.Navigator screenOptions={stackScreenOptions}>
+      <PokemonStack.Screen name="PokemonList" component={PokemonList} />
+      <PokemonStack.Screen name="PokemonDetail" component={PokemonDetail} />
+    </PokemonStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+function MoveStackScreen() {
+  return (
+    <MoveStack.Navigator screenOptions={stackScreenOptions}>
+      <MoveStack.Screen name="MoveList" component={MoveList} />
+      <MoveStack.Screen name="MoveDetail" component={MoveDetail} />
+    </MoveStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const ActiveColor = "#000000";
+const InActiveColor = "#00000077";
+const tabScreenOptions = ({ route }) => ({
+
+  headerShown: false,
+  tabBarActiveTintColor: ActiveColor,
+  tabBarInactiveTintColor: InActiveColor,
+  tabBarIcon: ({ color, size }) => {
+    return (
+      <View style={{ alignItems: "center" }}>
+        <Image
+          source={route.name === "Pokemons" ? pokemonTabIcon : moveTabIcon}
+          style={{
+            opacity: color == ActiveColor ? 1 : 0.5,
+            width: size,
+            height: size,
+          }}
+        />
+      </View>
+    );
   },
 });
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={tabScreenOptions}
+        >
+          <Tab.Screen name="Pokemons" component={PokemonStackScreen} />
+          <Tab.Screen name="Moves" component={MoveStackScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
